c87bb1fee9a832dbc04bb6f3dd88c000d5135421
wangjiandang <851355241@qq.com>
2018-02-28 10:40:16 +0800

    删除无用包

--- app/src/main/java/com/myfragmentdemo/home/HomeActivity.java
+++ app/src/main/java/com/myfragmentdemo/home/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.myfragmentdemo.home;
 
 import android.content.ComponentName;
-import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
